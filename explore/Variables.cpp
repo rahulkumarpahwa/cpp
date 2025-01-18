5a57aeabbebd3108403512a30f7cd0f193eb3d20
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-18 15:35:27 +0530

    starting file and variables

---
+++ explore/Variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int in = 10;
+    char ch = 'A';
+    float f = 10.99f; // f should be written to tell that it is float.
+    double d = 10.99;
+    bool b = true; // true is stored as 1 in memory.
+    cout << "integer : " << in << endl;
+    cout << "characterr : " << ch << endl;
+    cout << "float : " << f << endl;
+    cout << "double : " << d << endl;
+    cout << "bool : " << b << endl;
+    return 0;
+}
