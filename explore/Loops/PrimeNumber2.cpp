2e75e02f4614723a8da31d4d9aa692530443bde9
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-19 16:51:10 +0530

    Prime Number 2

---
+++ explore/Loops/PrimeNumber2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter any Number ";
+    cin >> n;
+    bool isPrime = true;
+    for (int i = 2; i *i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            isPrime = false;
+        }
+    }
+
+    if (isPrime)
+    {
+        cout << "The " << n << " is Prime";
+    }
+    else
+    {
+        cout << "The " << n << " is not Prime.";
+    }
+    return 0;
+}
