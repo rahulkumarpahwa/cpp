045bc9538e40c93132561b4f5ff06996f2bac791
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-19 16:54:52 +0530

    changes

--- explore/Loops/PrimeNumber2.cpp
+++ explore/Loops/PrimeNumber2.cpp
@@ -7,7 +7,7 @@ int main()
     cout << "Enter any Number ";
     cin >> n;
     bool isPrime = true;
-    for (int i = 2; i *i <= n; i++)
+    for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
         {
--- explore/Loops/SumOfEven.cpp
+++ explore/Loops/SumOfEven.cpp
@@ -12,4 +12,4 @@ int main()
 
     cout << "Sum is " << sum;
     return 0;
-}
+} 
