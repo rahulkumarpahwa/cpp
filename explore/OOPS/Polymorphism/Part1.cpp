19b738002ccf3f153f200a391e59d21ced8167e7
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-02-15 16:44:49 +0530

    Polymorphism intro

---
+++ explore/OOPS/Polymorphism/Part1.cpp
@@ -0,0 +1,22 @@
+// Polymorphism-Introduction
+// - one name and Many Forms
+// - eg. function overloading, operator overloading, virtual function, etc.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
+/*
+Polymorphism
+Types: 1. Compile time polymorphism : here we decide that at the compile time that which fxn is going to be called.
+eg. fxn overloading, operator overloading etc.
+Also called as early binding.
+
+2. RunTime polymorphism : here we will not decide at the compile time that which fxn is going to be called but at the runtime it is decided which fxn is going to be called.
+eg. virtual functions etc.
+also called as late binding.
+*/
